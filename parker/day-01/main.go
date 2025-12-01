91e0e6d1dc88499f7bb0b24b7115135fab358bb6
Parker Whitehead <pmw9905@gmail.com>
2025-12-01 06:21:57 -0600

    day1 part 1

--- parker/day-01/main.go
+++ parker/day-01/main.go
@@ -3,13 +3,54 @@ package main
 import (
 	"fmt"
 	"parker/aoc2025/util"
+	"strconv"
 )
+
+const TotalDialNumbers = 100
+const StartingDialPosition = 50
+
+func parseRotation(rotation string) (int, error) {
+	direction := rotation[0]
+	numStr := rotation[1:]
+
+	num, err := strconv.Atoi(numStr)
+
+	num = num % TotalDialNumbers
+
+	if direction == 'L' {
+		num = 100 - num
+	}
+	return num, err
+}
+
+func getPassword(turns []string) int {
+	password := 0
+	position := StartingDialPosition
+
+	for _, rotation := range turns {
+		num, err := parseRotation(rotation)
+
+		if err != nil {
+			panic(err)
+		}
+
+		position += num
+		position = (position) % TotalDialNumbers
+
+		if position == 0 {
+			password += 1
+		}
+	}
+	return password
+}
+
 func main() {
 	lines, err := utils.ReadInput("day-01/input.txt")
 
 	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(lines)
+		panic(err)
 	}
+
+	password := getPassword(lines)
+	fmt.Println(password)
 }
